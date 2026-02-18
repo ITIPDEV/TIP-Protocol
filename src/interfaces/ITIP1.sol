bede0ca9d94efd31dbadca7ec1b64443a6169b6b
ITIPDEV <261864101+ITIPDEV@users.noreply.github.com>
2026-02-18 14:02:26 +0800

    Create ITIP1.sol

---
+++ src/interfaces/ITIP1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface ITIP1 {
+    event Created(uint256 indexed id, address indexed controller);
+    
+    event Updated(uint256 indexed id, uint8 indexed oldState, uint8 indexed newState);
+
+    function register(uint256 id, address controller) external;
+    
+    function update(uint256 id, uint8 newState) external;
+    
+    function fetch(uint256 id) external view returns (address controller, uint8 state);
+}
